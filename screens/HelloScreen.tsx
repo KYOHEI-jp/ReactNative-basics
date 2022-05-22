465b0745e201a7ad70c7c2a34396f1121c24c6a1
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-05-22 13:13:36 +0900

    ReduxTKに遷移

--- screens/HelloScreen.tsx
+++ screens/HelloScreen.tsx
@@ -1,8 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { VFC, useEffect } from 'react'
 import { View, Text } from 'react-native'
 import tailwind from 'tailwind-rn'
+import { RootStackParamList } from '../types/types'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { Button } from 'react-native-elements'
 
-export const HelloScreen = () => {
+type Props = NativeStackScreenProps<RootStackParamList, 'Hello'>;
+
+export const HelloScreen: VFC<Props> = ({ navigation }) => {
     useEffect(() => {
         console.log("mounted Hello");
         return () => {
@@ -11,7 +16,11 @@ export const HelloScreen = () => {
     }, []);
     return (
         <View style={tailwind('flex-1 bg-gray-300 justify-center items-center')}>
-            <Text></Text>
+            <Text>Hello</Text>
+            <View style={tailwind("my-3")}>
+                <Button title="Go to ReduxTK"
+                    onPress={() => navigation.navigate("ReduxTK")} />
+            </View>
         </View>
     )
 }
