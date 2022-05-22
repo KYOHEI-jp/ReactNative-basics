a36f68c8db01bd4ef410d6e7ae58a11073c41bfa
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-05-22 13:06:40 +0900

    Helloに遷移

--- screens/FlexBoxScreen.tsx
+++ screens/FlexBoxScreen.tsx
@@ -1,8 +1,9 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import React, { VFC, useEffect } from 'react'
 import { View, Text } from 'react-native'
 import tailwind from 'tailwind-rn'
 import { RootStackParamList } from '../types/types'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { Button } from 'react-native-elements'
 
 type Props = NativeStackScreenProps<RootStackParamList, 'FlexBox'>;
 
@@ -15,7 +16,11 @@ export const FlexBoxScreen: VFC<Props> = ({ navigation }) => {
     }, []);
     return (
         <View style={tailwind('flex-1 bg-gray-300 justify-center items-center')}>
-            <Text></Text>
+            <Text>FlexBox</Text>
+            <View style={tailwind("my-3")}>
+                <Button title="Go to Hello"
+                    onPress={() => navigation.navigate("Hello")} />
+            </View>
         </View>
     )
 }
