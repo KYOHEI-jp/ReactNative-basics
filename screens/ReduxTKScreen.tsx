f0f5f492a090a690bf6bed890899d6b0fd0ea3ab
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-05-22 13:06:58 +0900

    FlexBoxに遷移

--- screens/ReduxTKScreen.tsx
+++ screens/ReduxTKScreen.tsx
@@ -1,8 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { VFC, useEffect } from 'react'
 import { View, Text } from 'react-native'
 import tailwind from 'tailwind-rn'
+import { RootStackParamList } from '../types/types'
+import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { Button } from 'react-native-elements'
 
-export const ReduxTKScreen = () => {
+
+type Props = NativeStackScreenProps<RootStackParamList, 'ReduxTK'>;
+
+export const ReduxTKScreen: VFC<Props> = ({ navigation }) => {
     useEffect(() => {
         console.log("mounted ReduxTK");
         return () => {
@@ -12,6 +18,10 @@ export const ReduxTKScreen = () => {
     return (
         <View style={tailwind('flex-1 bg-gray-300 justify-center items-center')}>
             <Text>Redux ToolKit</Text>
+            <View style={tailwind("my-3")}>
+                <Button title="Go to FlexBox"
+                    onPress={() => navigation.navigate("FlexBox")} />
+            </View>
         </View >
     )
 }
