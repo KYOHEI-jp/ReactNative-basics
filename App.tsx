c4241798a7e6cc632319ae62209f74106dcdc5ee
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-05-23 23:16:23 +0900

    Appにstore

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import { RootNavigator } from './navigations/RootNavigator';
 
 export default function App() {
-  return (
-    <RootNavigator />
+  return (<Provider store={store}><RootNavigator /></Provider>
+
     // flex-1でバックグラウンド全体、背景全部にgray-300、縦真ん中、横中央寄せ
     // <View style={tw('flex-1 bg-gray-300 justify-center items-center')}>
     //   <Text>Open up App.tsx to start working on your app!</Text>
