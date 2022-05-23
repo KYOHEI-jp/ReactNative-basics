69179b22282225ae3cd7eaf63a51fff4cc34c727
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-05-23 23:16:34 +0900

    ReduxToolKitにcount3を表示

--- screens/ReduxTKScreen.tsx
+++ screens/ReduxTKScreen.tsx
@@ -1,14 +1,16 @@
 import React, { VFC, useEffect } from 'react'
 import { View, Text } from 'react-native'
 import tailwind from 'tailwind-rn'
+import { useSelector, useDispatch } from 'react-redux'
 import { RootStackParamList } from '../types/types'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { Button } from 'react-native-elements'
-
+import { increment, decrement, addByPayload, selectCount, } from '../slices/counterSlice';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'ReduxTK'>;
 
 export const ReduxTKScreen: VFC<Props> = ({ navigation }) => {
+    const count = useSelector(selectCount);
     useEffect(() => {
         console.log("mounted ReduxTK");
         return () => {
@@ -17,7 +19,7 @@ export const ReduxTKScreen: VFC<Props> = ({ navigation }) => {
     }, []);
     return (
         <View style={tailwind('flex-1 bg-gray-300 justify-center items-center')}>
-            <Text>Redux ToolKit</Text>
+            <Text>Redux ToolKit {count}</Text>
             <View style={tailwind("my-3")}>
                 <Button title="Go to FlexBox"
                     onPress={() => navigation.navigate("FlexBox")} />
