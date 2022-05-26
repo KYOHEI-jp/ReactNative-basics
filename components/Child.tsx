320587de96007a96dc4234d9ddbe5b5b73db5a08
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-05-26 21:29:18 +0900

    VFC: Props

--- components/Child.tsx
+++ components/Child.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { VFC, memo } from 'react';
 import tailwind from 'tailwind-rn';
 import { View, Text } from 'react-native'
+import { Button } from "react-native-elements"
+type Props = {
+    printMsg: () => void;
+}
 
-export const Child = () => {
+const ChildMemo: VFC<Props> = ({ printMsg }) => {
     console.log("child rendered")
     return (
         <View>
             <Text style={tailwind("my-3")}>Child</Text>
+            <Button title="Print" onPress={printMsg} />
         </View>
     )
 }
+
+export const Child = memo(ChildMemo);
